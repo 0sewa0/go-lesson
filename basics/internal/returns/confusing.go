61f8cc0aac6068acb247f2c5de08aac5f91ae168
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-01-11 08:22:23 +0100

    More stuff

--- basics/internal/returns/confusing.go
+++ basics/internal/returns/confusing.go
@@ -5,6 +5,13 @@ func Annoying() (string, string, int) {
 	return "elephant", "Bob", 5
 }
 
+func LessAnnoying() (type_ string, name string, weight int) {
+	type_ = "parrot"
+	name = "Steven"
+	weight = 1
+	return
+}
+
 type Animal struct{
 	Type string
 	Name string
