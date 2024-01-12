571286e3a6b3a984510a1050b7fd103d9307a909
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-01-12 15:06:40 +0100

    WIP for the "advanced" class (adds errors)

--- advanced/internal/snowman/types.go
+++ advanced/internal/snowman/types.go
@@ -1,5 +1,7 @@
 package snowman
 
+import "fmt"
+
 const (
 	maxNameLength = 10
 )
@@ -8,9 +10,31 @@ type Melter interface {
 	Melt(int) error
 }
 
+type TooColdToMeltError string
+
+func (cold TooColdToMeltError) Error() string {
+	return fmt.Sprintf("That is too cold to melt anything! %s lives another day", cold)
+}
+
+// ---
+
 type Hugger interface {
 	Hug(int) error
 }
+
+type TooSmallToHugError string
+
+func (small TooSmallToHugError) Error() string {
+	return fmt.Sprintf("You are too small to hug %s! Disappointing", small)
+}
+
+type GotStabbedError string
+
+func (stab GotStabbedError) Error() string {
+	return fmt.Sprintf("You got stabbed in the eye by %s's nose! You are a pirate now", stab)
+}
+
+// ---
 
 type nose struct {
 	material NoseMaterial
@@ -31,9 +55,20 @@ type snowman struct {
 }
 
 func (s snowman) Melt(temp int) error {
-	return nil // TODO: Add TooColdError
+	if temp < 0 {
+		return TooColdToMeltError(s.name)
+	}
+	fmt.Printf("You melted %s, how could you!?", s.name)
+	return nil
 }
 
-func (s snowman) Hug(armLength int) error {
-	return nil // TODO: Add TooSmallError
+func (s snowman) Hug(armLength uint) error {
+	if s.size < s.nose.length {
+		return GotStabbedError(s.name)
+	}
+	if armLength < s.size/2 {
+		return TooSmallToHugError(s.name)
+	}
+	fmt.Printf("You hugged %s, nice.", s.name)
+	return nil
 }
